218688967b917f9afcbe20ae5930fd6b106a4dd5
ikram benouaklil <121358155+ikrambenouaklil@users.noreply.github.com>
2024-06-03 13:00:52 +0100

    ..

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const app = express();
 const PORT = 3000;
 const userRouter = require("./routes/user")
-app.use(cors())
+app.use(cors());
 app.use(express.json())
 
   mongoose.connect(
